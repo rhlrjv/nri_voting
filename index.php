9dd9fb99580a69865fc178686a43538ce8cc3302
Rahul Rajeev <rahulrajeev91@gmail.com>
2014-04-27 19:38:02 +0800

    updated php to add db

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-<html>
-	<body>
-		<h1>IT WORKS !!!!</h1>
-	</body>
-</html>
+<?php
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+function pg_connection_string() {
+  // we will fill this out next
+}
+ 
+# Establish db connection
+$db = pg_connect(pg_connection_string());
+if (!$db) {
+    echo "Database connection error."
+    exit;
+}
+ 
+$result = pg_query($db, "SELECT statement goes here");
+
+	echo "hello";
+?>
