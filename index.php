9026b65e1b9619972c86a2a766d4eca965681874
Rahul Rajeev <rahulrajeev91@gmail.com>
2014-04-28 09:58:56 +0800

    added facebook sdk

--- index.php
+++ index.php
@@ -1,18 +1,48 @@
 <?php
-# This function reads your DATABASE_URL configuration automatically set by Heroku
-# the return value is a string that will work with pg_connect
-function pg_connection_string() {
-  // we will fill this out next
-}
- 
-# Establish db connection
-$db = pg_connect(pg_connection_string());
-if (!$db) {
-    echo "Database connection error."
-    exit;
-}
- 
-$result = pg_query($db, "SELECT statement goes here");
 
-	echo "hello";
+  require_once 'facebook-sdk/facebook.php';
+
+  // Create our Application instance (replace this with your appId and secret).
+  $facebook = new Facebook(array(
+    'appId'  => '621299604622729',
+    'secret' => 'c0910de142c84818c500a12be3091067',
+  ));
+
+  $user_id = $facebook->getUser();
 ?>
+<html>
+  <head></head>
+  <body>
+
+  <?php
+    if($user_id) {
+
+      // We have a user ID, so probably a logged in user.
+      // If not, we'll get an exception, which we handle below.
+      try {
+
+        $user_profile = $facebook->api('/me','GET');
+        echo "Name: " . $user_profile['name'];
+
+      } catch(FacebookApiException $e) {
+        // If the user is logged out, you can have a 
+        // user ID even though the access token is invalid.
+        // In this case, we'll get an exception, so we'll
+        // just ask the user to login again here.
+        $login_url = $facebook->getLoginUrl(); 
+        echo 'Please <a href="' . $login_url . '">login.</a>';
+        error_log($e->getType());
+        error_log($e->getMessage());
+      }   
+    } else {
+
+      // No user, print a link for the user to login
+      $login_url = $facebook->getLoginUrl();
+      echo 'Please <a href="' . $login_url . '">login.</a>';
+
+    }
+
+  ?>
+
+  </body>
+</html>
